aa22bff313152d61a3eae57f759af2b393a4f0ae
EVolgina <123949683+EVolgina@users.noreply.github.com>
2024-05-25 18:33:34 +0500

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,1 +1,20 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+      version = "~> 0.69"
+    }
+  }
+  required_version = ">=0.13"
+#  backend "local" {
+#    path = "terraform.tfstate"
+#  }
+}
+
+provider "yandex" {
+  token     = var.token
+  cloud_id  = var.cloud_id
+  folder_id = var.folder_id
+  zone      = var.zone
+}
 
